865cc704d2e82a05b6727f9340aad7de6f7f5549
cleisonmp <cleison.mp@gmail.com>
2022-12-06 15:47:18 -0300

    project setup :tada:

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    '@typescript-eslint/consistent-type-definitions': ['warn'],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/prefer-ts-expect-error': 'warn',
+
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        'checksVoidReturn': false,
+      },
+    ],
+    '@typescript-eslint/prefer-includes': 'error',
+    '@typescript-eslint/prefer-function-type': 'warn',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
+  },
+}
---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import type { TestingModule } from '@nestjs/testing'
+import { Test } from '@nestjs/testing'
+import type { INestApplication } from '@nestjs/common'
+import * as request from 'supertest'
+import { AppModule } from './../src/app.module'
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+    prisma = app.get<PrismaService>(PrismaService)
+
+    useContainer(app.select(AppModule), { fallbackOnErrors: true })
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+    await app.init()
+  })
+})

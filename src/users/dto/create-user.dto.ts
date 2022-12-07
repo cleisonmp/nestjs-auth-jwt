26ac5b6af2b8e8e668ff9b03ee3b717e6bec7f8f
cleisonmp <cleison.mp@gmail.com>
2022-12-07 13:59:51 -0300

    feat: add password strength validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Transform } from 'class-transformer'
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator'
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -25,5 +31,12 @@ export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
   @MinLength(8)
+  // Passwords will contain at least 1 upper case letter
+  // Passwords will contain at least 1 lower case letter
+  // Passwords will contain at least 1 number or special character
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Passwords must contain: at least 1 upper case letter AND 1 lower case letter AND 1 number or special character',
+  })
   password: string
 }

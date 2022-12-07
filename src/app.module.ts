e919703968c22714e8f5ab23dc6f4b098af8fad5
cleisonmp <cleison.mp@gmail.com>
2022-12-07 17:26:40 -0300

    refactor: organize imports :recycle:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common'
+
 import { PrismaModule } from './prisma/prisma.module'
+import { AuthModule } from './auth/auth.module'
+
 import { UsersModule } from './users/users.module'
 import { CategoriesModule } from './categories/categories.module'
 import { PostsModule } from './posts/posts.module'
-import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
     PrismaModule,
+    AuthModule,
     UsersModule,
     CategoriesModule,
     PostsModule,
-    AuthModule,
   ],
   controllers: [],
   providers: [],

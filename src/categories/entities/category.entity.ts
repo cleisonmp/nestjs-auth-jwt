e8002835070d6788551dfd4f9366cabe489c6eee
cleisonmp <cleison.mp@gmail.com>
2022-12-08 19:27:47 -0300

    docs: improve documentarion :memo:

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -2,15 +2,20 @@ import { ApiProperty } from '@nestjs/swagger'
 import type { Category } from '@prisma/client'
 
 export class CategoryEntity implements Category {
-  @ApiProperty()
+  @ApiProperty({
+    example: '10ea9657-0255-4175-b610-00ea1379b5a8',
+  })
   id: string
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Category name',
+    example: 'Technology',
+  })
   name: string
 
-  @ApiProperty()
+  @ApiProperty({ example: '2022-12-08T20:18:57.135Z' })
   createdAt: Date
 
-  @ApiProperty()
+  @ApiProperty({ example: '2022-12-08T20:18:57.135Z' })
   updatedAt: Date
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,22 +3,30 @@ import type { User } from '@prisma/client'
 import { Exclude } from 'class-transformer'
 
 export class UserEntity implements User {
-  @ApiProperty()
+  @ApiProperty({
+    example: '10ea9657-0255-4175-b610-00ea1379b5a8',
+  })
   id: string
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'User full name',
+    example: 'John Doe',
+  })
   name: string
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Email address of the user',
+    example: 'mail@mail.com',
+  })
   email: string
 
   @Exclude()
   password: string
 
-  @ApiProperty()
+  @ApiProperty({ example: '2022-12-08T20:18:57.135Z' })
   createdAt: Date
 
-  @ApiProperty()
+  @ApiProperty({ example: '2022-12-08T20:18:57.135Z' })
   updatedAt: Date
 
   constructor(partial: Partial<UserEntity>) {
